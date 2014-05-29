ee6c38b3b32aa6c62ae54cd67c5426ae3b9279b5
Adam Doherty <adam.doherty@cfsgeeks.com>
2014-05-29 10:44:22 -0500

    External Includes

--- templatetags/site_extras.py
+++ templatetags/site_extras.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.simple_tag
 def autoversion(filename):
 	return int(os.path.getmtime(filename))
+
+@register.simple_tag
+def include_external(url):
+    import urllib2
+    return urllib2.urlopen(url).read()
